b394dcf8dd53279d293672af057171078740256c
Aitor <aitordediego23@gmail.com>
2025-01-06 18:08:45 +0100

    [Dev] Added Literal types code examples

--- typescript/courses/understanding-typescript/project/main.ts
+++ typescript/courses/understanding-typescript/project/main.ts
@@ -18,3 +18,36 @@ console.log(combinedAges);
 const combinedNames = combine('Aitor', 'Clara');
 
 console.log(combinedNames);
+
+
+// Literal type example
+function combine2(
+	input1: number | string,
+	input2: number | string,
+	resultConversion: 'as-number' | 'as-text'
+) {
+	let result: number | string;
+
+	if (
+		(typeof input1 === 'string' && typeof input2 === 'string') ||
+		resultConversion === 'as-text'
+	) {
+		result = input1.toString() + input2.toString();
+	} else {
+		result = +input1 + +input2;
+	}
+
+	return result;
+}
+
+const combinedAges2 = combine2(31, 25, 'as-number');
+
+console.log(combinedAges2);
+
+const combinedStringAges = combine2('31', '25', 'as-number');
+
+console.log(combinedStringAges);
+
+const combinedNames2 = combine2('Aitor', 'Clara', 'as-text');
+
+console.log(combinedNames2);
